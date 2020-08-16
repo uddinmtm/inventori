c6afca0931480038b705de746e282b7fb8322ebd
Muhammad Sapuan <muhammadsapuan16@gmail.com>
2020-08-16 15:26:47 +0700

    tambah script update & delete

--- public/actions/users.php
+++ public/actions/users.php
@@ -7,39 +7,110 @@ parse_str(file_get_contents("php://input"), $params);
 
 // add
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (empty($_POST['name'])) {
+    if (empty($params['name'])) {
         echo json_encode(['message' => 'nama required']);
         http_response_code(400);
         die();
     }
 
-    if (empty($_POST['username'])) {
+    if (empty($params['username'])) {
         echo json_encode(['message' => 'username required']);
         http_response_code(400);
         die();
     }
 
-    if (empty($_POST['password'])) {
+    if (empty($params['password'])) {
         echo json_encode(['message' => 'password required']);
         http_response_code(400);
         die();
     }
 
-    if (empty($_POST['level'])) {
+    if (empty($params['level'])) {
         echo json_encode(['message' => 'level required']);
         http_response_code(400);
         die();
     }
 
-    $name       = mysqli_real_escape_string($conn, $_POST['name']);
-    $username   = mysqli_real_escape_string($conn, $_POST['username']);
-    $password   = mysqli_real_escape_string($conn, $_POST['password']);
-    $level      = mysqli_real_escape_string($conn, $_POST['level']);
+    $name       = mysqli_real_escape_string($conn, $params['name']);
+    $username   = mysqli_real_escape_string($conn, $params['username']);
+    $password   = mysqli_real_escape_string($conn, $params['password']);
+    $level      = mysqli_real_escape_string($conn, $params['level']);
 
-    $sql = "INSERT INTO m_user (id, name, `level`, username, password, created_by) VALUES ('','{$name}', '{$level}', '{$username}', '{$password}', '{$_SESSION['username']}')";
+    $sql = "INSERT INTO m_user (name, level, username, password, created_by) VALUES ('{$name}', '{$level}', '{$username}', '{$password}', '{$_SESSION['username']}')";
     $result = mysqli_query($conn, $sql);
     if (!$result) {
         echo json_encode(['message' => 'save item failed']);
+        http_response_code(400);
+        die();
+    }
+
+    echo json_encode(['status' => 'success']);
+    die();
+}
+
+// update 
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    if (empty($params['id'])) {
+        echo json_encode(['message' => 'id required']);
+        http_response_code(400);
+        die();
+    }
+
+    if (empty($params['name'])) {
+        echo json_encode(['message' => 'nama required']);
+        http_response_code(400);
+        die();
+    }
+
+    if (empty($params['username'])) {
+        echo json_encode(['message' => 'username required']);
+        http_response_code(400);
+        die();
+    }
+
+    if (empty($params['password'])) {
+        echo json_encode(['message' => 'password required']);
+        http_response_code(400);
+        die();
+    }
+
+    if (empty($params['level'])) {
+        echo json_encode(['message' => 'level required']);
+        http_response_code(400);
+        die();
+    }
+
+    $id         = mysqli_real_escape_string($conn, $params['id']);
+    $name       = mysqli_real_escape_string($conn, $params['name']);
+    $username   = mysqli_real_escape_string($conn, $params['username']);
+    $password   = mysqli_real_escape_string($conn, $params['password']);
+    $level      = mysqli_real_escape_string($conn, $params['level']);
+
+    $sql = "UPDATE m_user SET name='{$name}', level='{$level}', username='{$username}', password='{$password}', updated_by='{$_SESSION['username']}' WHERE id={$id}";
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo json_encode(['message' => 'update item failed']);
+        http_response_code(400);
+        die();
+    }
+
+    echo json_encode(['status' => 'success']);
+    die();
+}
+
+// delete
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    if (empty($_REQUEST['id'])) {
+        echo json_encode(['message' => 'id required']);
+        http_response_code(400);
+        die();
+    }
+
+    $id = mysqli_real_escape_string($conn, $_REQUEST['id']);
+    $sql = "DELETE FROM m_user WHERE id={$id}";
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo json_encode(['message' => 'delete item failed']);
         http_response_code(400);
         die();
     }
