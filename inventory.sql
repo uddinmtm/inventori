235f1ab3cdd5704f2f705bd296e93e7c2d652d81
Uddin Mtm <saifuddinmutammam@gmail.com>
2020-11-11 07:50:27 +0700

    remove comment

--- inventory.sql
+++ inventory.sql
@@ -1,27 +1,3 @@
--- MariaDB dump 10.17  Distrib 10.5.6-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: inventory
--- ------------------------------------------------------
--- Server version	10.5.6-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `m_item`
---
-
-DROP TABLE IF EXISTS `m_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -35,17 +11,8 @@ CREATE TABLE `m_item` (
   KEY `m_item_FK_1` (`updated_by`),
   CONSTRAINT `m_item_FK` FOREIGN KEY (`created_by`) REFERENCES `m_user` (`username`),
   CONSTRAINT `m_item_FK_1` FOREIGN KEY (`updated_by`) REFERENCES `m_user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
---
--- Table structure for table `m_user`
---
-
-DROP TABLE IF EXISTS `m_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -59,25 +26,9 @@ CREATE TABLE `m_user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `m_user_UN` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `m_user`
---
-
-LOCK TABLES `m_user` WRITE;
-/*!40000 ALTER TABLE `m_user` DISABLE KEYS */;
 INSERT INTO `m_user` VALUES (1,'Super Admin','super_admin','admin','21232f297a57a5a743894a0e4a801fc3','2020-07-25 09:29:47','admin',NULL,'admin'),(2,'Manager','manager','manager','manager','2020-11-08 09:12:34','admin',NULL,NULL);
-/*!40000 ALTER TABLE `m_user` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `t_stock`
---
-
-DROP TABLE IF EXISTS `t_stock`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_stock` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `transaction_code` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -93,16 +44,8 @@ CREATE TABLE `t_stock` (
   KEY `t_stock_FK_1` (`updated_by`),
   CONSTRAINT `t_stock_FK` FOREIGN KEY (`created_by`) REFERENCES `m_user` (`username`),
   CONSTRAINT `t_stock_FK_1` FOREIGN KEY (`updated_by`) REFERENCES `m_user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Table structure for table `t_stock_detail`
---
-
-DROP TABLE IF EXISTS `t_stock_detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_stock_detail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `transaction_code` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -114,19 +57,4 @@ CREATE TABLE `t_stock_detail` (
   PRIMARY KEY (`id`),
   KEY `t_stock_detail_FK` (`item_id`),
   CONSTRAINT `t_stock_detail_FK` FOREIGN KEY (`item_id`) REFERENCES `m_item` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping routines for database 'inventory'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
